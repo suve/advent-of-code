aa3b42094813fe4fd8046bab43c14cb7fdabc54a
suve <veg@svgames.pl>
2019-12-04 10:58:22 +0100

    Add solution for 2019/day04/part2

--- 2019/day04/password.sh
+++ 2019/day04/password.sh
@@ -3,9 +3,10 @@
 MINIMUM=382345
 MAXIMUM=843167
 
-seq 1 999999 | \
-	head -n "$MAXIMUM" | \
-	tail -n "+$MINIMUM" | \
+PART1=$(mktemp "aoc.day04.part1.XXXXXX")
+PART2=$(mktemp "aoc.day04.part2.XXXXXX")
+
+seq "$MINIMUM" "$MAXIMUM" | \
 	grep -E -e '(11|22|33|44|55|66|77|88|99)' | \
 	grep -v -E -e '(10)' | \
 	grep -v -E -e '(20|21)' | \
@@ -15,5 +16,17 @@ seq 1 999999 | \
 	grep -v -E -e '(60|61|62|63|64|65)' | \
 	grep -v -E -e '(70|71|72|73|74|75|76)' | \
 	grep -v -E -e '(80|81|82|83|84|85|86|87)' | \
-	grep -v -E -e '(90|91|92|93|94|95|96|97|98)' | \
-	wc -l
+	grep -v -E -e '(90|91|92|93|94|95|96|97|98)' > "$PART1"
+
+REGEX=""
+for D in $(seq 1 9); do
+	REGEX="${REGEX}|((^|[^$D]{1})$D{2}([^$D]{1}|\$))"
+done
+REGEX=${REGEX:1:999999}
+
+grep -E -e "$REGEX" < "$PART1" > "$PART2"
+
+wc -l "$PART1"
+wc -l "$PART2"
+
+rm "$PART1" "$PART2"
