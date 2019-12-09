99ad7cddffb9727d4384b3ec77594e85759ffab1
suve <veg@svgames.pl>
2019-12-09 15:04:44 +0100

    Add some error handling to the README generator

--- .readme/generate.php
+++ .readme/generate.php
@@ -1,12 +1,31 @@
 #!/usr/bin/php
 <?php
 
+function read_file($path) {
+	$data = file_get_contents($path);
+	if($data === FALSE) {
+		fprintf(STDERR, "Failed to read file \"%s\"\n", $data);
+		exit(1);
+	}
+	return $data;
+}
+
+function read_json_file($path) {
+	$data = read_file($path);
+	$data = json_decode($data, true);
+	if(json_last_error() != JSON_ERROR_NONE) {
+		fprintf(STDERR, "Failed to read JSON from file \"%s\": %s\n", json_last_error_msg());
+		exit(1);
+	}
+	return $data;
+}
+
+
 chdir(__DIR__);
 
-$languages = json_decode(file_get_contents('languages.json'), true);
-$solutions = json_decode(file_get_contents('solutions.json'), true);
-
-$header = file_get_contents('header.md');
+$languages = read_json_file('languages.json');
+$solutions = read_json_file('solutions.json');
+$header = read_file('header.md');
 
 echo $header;
 
@@ -28,9 +47,14 @@ for($i = 0; $i < 49; ++$i) {
 	$dayNo = floor(($i / 2) + 1);
 	$daySymbol = $dayNo . (($i % 2) ? '½' : '');
 	
-	echo "| $daySymbol |";
+	echo "| **$daySymbol** |";
 	
 	foreach($solutions as $year => $files) {
+		if($i >= count($files)) {
+			echo ' |';
+			continue;
+		}
+
 		$file = $files[$i];
 		if($file === NULL) {
 			echo ' |';
@@ -38,8 +62,12 @@ for($i = 0; $i < 49; ++$i) {
 		}
 		
 		$ext = pathinfo($file, PATHINFO_EXTENSION);
+		if(!isset($languages[$ext])) {
+			fprintf(STDERR, "Unknown file extension \"%s\" (file: \"%s\")\n", $ext, $file);
+			exit(1);
+		}
+
 		$lang = $languages[$ext];
-		
 		$path = $year . '/day' . ($dayNo < 10 ? '0' : '') . $dayNo . '/' . $file;
 		
 		echo " [![$lang](https://github.com/suve/advent-of-code/raw/master/.readme/$ext.png)]($path) |";
